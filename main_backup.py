50bfb04764e8dfaed66763bb9fb3a8616fc53f56
Aubaldullah Madoseh <abdullahubaidillah96@gmail.com>
2026-07-15 20:43:04 +0800

    refactor: create application flow with CRUD menu

---
+++ main_backup.py
@@ -0,0 +1,440 @@
+#  BAHAGIAN BUSINESS LOGIC IS BASED APA YANG AKU FAHAM UNTUK MASALAH APA YANG AKU NAK OVERCOME .
+# AI HANYA BAGI CODE, LOGIC IS FROM ME.
+## main_backup.py, sekarang nak refactor, in case apa2 terjadi, boleh rujuk code asal
+# from api import get_match
+from api import get_match, get_heroes, get_items
+from analyzer import (find_player_by_hero, display_performance, farming_analysis, kda_analysis, display_items, threat_analysis, benchmark_analysis, generate_coach_report)
+from analyzer import get_enemy_players
+from analyzer import item_recommendation
+from graph import create_benchmark_chart
+from input_handler import get_user_input
+from database import create_table
+from database import create_table, save_analysis
+from database import view_history
+from database import delete_analysis
+from database import add_notes_column
+from database import update_note
+
+def analyse_match():
+
+    print("\n====== NEW MATCH ANALYSIS ======")
+
+
+    match_id, target_hero = get_user_input()
+
+
+    data = get_match(match_id)
+
+
+    if not data:
+
+        print("Match data not found.")
+        return
+
+
+    heroes = get_heroes()
+    items = get_items()
+
+
+    hero_map = {}
+
+    for hero in heroes:
+
+        hero_map[hero["id"]] = hero["localized_name"]
+
+
+    item_map = {}
+
+    for item_name, item_data in items.items():
+
+        if "id" in item_data and "dname" in item_data:
+
+            item_map[item_data["id"]] = item_data["dname"]
+
+
+    player = find_player_by_hero(
+        data["players"],
+        hero_map,
+        target_hero
+    )
+
+
+    if player is None:
+
+        print("Player not found.")
+        return
+
+
+    display_performance(player)
+
+    farming_analysis(player)
+
+    kda_analysis(player)
+
+
+    benchmark_result = benchmark_analysis(player)
+
+
+    save_analysis(
+        match_id,
+        target_hero,
+        player.get(
+            "personaname",
+            "Unknown"
+        ),
+        benchmark_result["KDA"]["player"],
+        benchmark_result["Farming"]["player"],
+        player["gold_per_min"],
+        benchmark_result["Farming"]["status"]
+    )
+
+
+    generate_coach_report(
+        benchmark_result,
+        player
+    )
+
+
+    create_benchmark_chart(
+        benchmark_result
+    )
+
+
+    display_items(
+        player,
+        item_map
+    )
+
+
+    enemy_players = get_enemy_players(
+        data["players"],
+        player
+    )
+
+
+    threat_analysis(
+        enemy_players,
+        hero_map
+    )
+
+
+    item_recommendation(
+        enemy_players,
+        hero_map
+    )
+
+def show_menu():
+
+    print("\n====== DOTA POS 1 COACH ======")
+
+    print("1. Analyse New Match")
+    print("2. View History")
+    print("3. Update Reflection")
+    print("4. Delete Analysis")
+    print("5. Exit")
+
+# def analyse_match():
+
+#     print("\n====== NEW MATCH ANALYSIS ======")
+
+#     match_id = int(
+#         input("Enter Match ID: ")
+#     )
+
+#     target_hero = input(
+#         "Enter Carry Hero: "
+#     )
+
+
+    # data = get_match(match_id)
+
+    # heroes = get_heroes()
+
+    # items = get_items()
+
+    # print("TARGET HERO:", target_hero)
+
+# def show_menu():
+
+#     print("\n====== DOTA POS 1 COACH ======")
+
+#     print("1. Analyse New Match")
+#     print("2. View Analysis History")
+#     print("3. Update Reflection Note")
+#     print("4. Delete Analysis")
+#     print("5. Exit")
+
+
+# match_id = 8881925243
+
+# add_notes_column()
+
+# create_table()
+
+# match_id, target_hero = get_user_input()
+
+# match_id = int(
+#     input("Enter Match ID: ")
+# )
+
+# data = get_match(match_id)
+
+# if not data:
+
+#     print(
+#         "Match data not found."
+#     )
+
+#     exit()
+
+# heroes = get_heroes()
+# items = get_items()
+
+
+# hero_map ={}
+
+# for hero in heroes:
+#     hero_map[hero["id"]] = hero ["localized_name"]
+
+# for player in data["players"]:
+#     hero_name = hero_map[player["hero_id"]]
+#     print(
+#         player["personaname"],
+#         "|",
+#         hero_name,
+#         "|",
+#         player["kills"],
+#         player["deaths"],
+#         player["assists"]
+#     )
+
+# item_map = {}
+
+# for item_name, item_data in items.items():
+
+# #   print(items["blink"])
+
+# #   item_map[item_data["id"]] = item_data["localized_name"]
+
+# ## selepas diuji berkali2, ketika mencari items, format localized tidak boleh dipakai .
+
+# # SOLUSI :
+
+#     if "id" in item_data and "dname" in item_data:
+#         item_map[item_data["id"]] = item_data["dname"] # kat sini python akan cek guna dua konsep, id dan dname, if ada print out,if takde skip.
+
+#  print(item_map[1]) # testing output :
+
+# print(heroes[0])
+
+
+
+# if data: 
+#     print("Api Connected")
+#     print(data["players"][0]) # cek type data yang dibentangkan, guna data.keys
+
+# else:
+#     print("Failed")
+
+# for player in data["players"]:
+#     print(
+#         player["personaname"],
+#         player["hero_id"],
+#         player["kills"],
+#         player["deaths"],
+#         player["assists"]
+#     )
+
+# target_hero = "spectre"
+
+# target_hero = input(
+#     "Enter Carry Hero: "
+# )
+
+# player =_hero # data = get_match(match_id)
+
+
+# if player is None:
+
+#     print(
+#         "\nPlayer not found."
+#     )
+
+#     print(
+#         "Please check hero name or match ID."
+#     )
+
+#     exit()
+
+# print("TARGET HERO:", target_hero)
+
+# print("\n=== HERO DALAM MATCH ===")
+
+# for p in data["players"]:
+#     print(hero_map[p["hero_id"]])
+
+# print("PLAYER CHECK:")
+# print(player)
+
+# display_performance(player)
+
+# farming_analysis(player)
+
+# kda_analysis(player)
+
+# benchmark_result = benchmark_analysis(player)
+
+# kda_value = benchmark_result["KDA"]["player"]
+
+# lh_value = benchmark_result["Farming"]["player"]
+
+# status = benchmark_result["Farming"]["status"]
+
+# save_analysis(
+
+#     match_id,
+
+#     target_hero,
+
+#     player.get(
+#         "personaname",
+#         "Unknown"
+#     ),
+
+#     kda_value,
+
+#     lh_value,
+
+#     player["gold_per_min"],
+
+#     status
+
+# )
+
+# generate_coach_report(
+#     benchmark_result,
+#     player
+# )
+
+
+# create_benchmark_chart(
+#     benchmark_result
+# )
+
+## report or summary pertama is done : match id + hero = display performance.
+
+# display_items(player, item_map)
+# dah keluar output pasal item
+
+# print(player["purchase_log"]) # testing purchase log item player
+# output : keyerror.
+
+# print(player.keys()) # testing untuk tengok key yang ada 
+# output : mmg takde bagi item history dengn cara ini.
+
+# display_items(player, item_map)
+
+# enemy_players = get_enemy_players(
+#     data["players"],
+#     player
+# )
+
+# threat_analysis(enemy_players, hero_map)
+
+# item_recommendation(enemy_players, hero_map)
+
+# print("\n====== ENEMY TEAM ======")
+
+# for enemy in enemy_players:
+
+#     player_name = enemy.get("personaname", "Unknown Player")
+#     hero_name = hero_map[enemy["hero_id"]]
+
+#     print(f"{player_name} | {hero_name}")
+
+## problems : OpenDota API returns hero information as hero_id instead of hero name.
+## solutions : Created hero mapping dictionary to convert hero_id into readable hero name.
+
+# print("\n====== ANALYSIS HISTORY ======")
+
+# history = view_history()
+
+# print("\n====== ANALYSIS HISTORY ======")
+
+# for record in history:
+
+#     print(f"\nID: {record[0]}")
+#     print(f"Match ID: {record[1]}")
+#     print(f"Hero: {record[2].title()}")
+#     print(f"Player: {record[3]}")
+#     print(f"KDA: {record[4]}")
+#     print(f"LH/min: {record[5]}")
+#     print(f"GPM: {record[6]}")
+#     print(f"Status: {record[7]}")
+
+#     print("-" * 30)
+
+# delete_analysis(2) ## testing deleting function
+
+# print("\nRecord ID 2 deleted.")
+
+# note = input(
+#     "\nAdd your match reflection note: "
+# )
+
+
+# update_note(
+#     1,
+#     note
+# ) find_player_by_hero(
+#     data["players"],
+#     hero_map,
+#     target
+
+while True:
+
+    show_menu()
+
+    choice = input(
+        "\nChoose option: "
+    )
+
+
+    if choice == "1":
+
+        print("Starting analysis...")
+
+
+    elif choice == "2":
+
+        print("Show history...")
+
+
+    elif choice == "3":
+
+        print("Update note...")
+
+
+    elif choice == "4":
+
+        print("Delete analysis...")
+
+
+    elif choice == "5":
+
+        print("Exit program")
+        break
+
+
+    else:
+
+        print("Invalid option")
+
+
+
+if __name__ == "__main__":
+
+    create_table()
+    add_notes_column()
+
+    analyse_match()
---
+++ main_v2.py
@@ -0,0 +1,320 @@
+# DOTA POS 1 COACH
+# Version 0.6 Refactor
+
+
+# =========================
+# IMPORT
+# =========================
+
+from api import get_match, get_heroes, get_items
+
+from analyzer import (
+    find_player_by_hero,
+    display_performance,
+    farming_analysis,
+    kda_analysis,
+    display_items,
+    threat_analysis,
+    benchmark_analysis,
+    generate_coach_report,
+    get_enemy_players,
+    item_recommendation
+)
+
+from graph import create_benchmark_chart
+
+from input_handler import get_user_input
+
+from database import (
+    create_table,
+    save_analysis,
+    view_history,
+    delete_analysis,
+    add_notes_column,
+    update_note
+)
+
+## GETTING USER DETAILS AND DISPLAY IT ##
+
+def analyse_match():
+
+    print("\n====== NEW MATCH ANALYSIS ======")
+
+    ### INPUT ID
+
+    match_id, target_hero = get_user_input()
+
+
+    data = get_match(match_id)
+
+
+    if not data:
+
+        print("Match data not found.")
+        return
+
+
+    heroes = get_heroes()
+
+    items = get_items()
+
+    #### FINDING HERO
+
+    hero_map = {}
+
+
+    for hero in heroes:
+
+        hero_map[hero["id"]] = hero["localized_name"]
+    
+    ### FINDING ITEM
+    
+    item_map = {}
+
+
+    for item_name, item_data in items.items():
+
+        if "id" in item_data and "dname" in item_data:
+
+            item_map[item_data["id"]] = item_data["dname"]
+    
+    ### FINDING PLAYER HERO
+
+    player = find_player_by_hero(
+        data["players"],
+        hero_map,
+        target_hero
+        )
+    
+
+    if player is None:
+
+        print(
+            "\nPlayer not found."
+        )
+
+        print(
+            "Please check hero name or match ID."
+        )
+
+        return
+
+    print(
+        "TARGET HERO:",
+        target_hero
+        )
+    
+    ## DISPLAY DATA ##
+    
+    display_performance(player)
+
+    farming_analysis(player)
+
+    kda_analysis(player)
+
+    benchmark_result = benchmark_analysis(player)
+
+    generate_coach_report(
+        benchmark_result,
+        player
+    )
+
+    ### SAVING RECORD
+
+    save_analysis(
+
+        match_id,
+
+        target_hero,
+
+        player.get(
+            "personaname",
+            "Unknown"
+        ),
+
+        benchmark_result["KDA"]["player"],
+
+        benchmark_result["Farming"]["player"],
+
+        player["gold_per_min"],
+
+        benchmark_result["Farming"]["status"]
+
+    )
+
+    create_benchmark_chart(
+        benchmark_result
+    )
+
+    display_items(
+        player,
+        item_map
+    )
+
+    enemy_players = get_enemy_players(
+        data["players"],
+        player
+    )
+
+    print("\n====== ENEMY TEAM ======")
+
+
+    for enemy in enemy_players:
+
+        player_name = enemy.get(
+            "personaname",
+            "Unknown Player"
+        )
+
+        hero_name = hero_map[
+            enemy["hero_id"]
+        ]
+
+        print(
+            f"{player_name} | {hero_name}"
+        )
+
+    enemy_players = get_enemy_players(
+        data["players"],
+        player
+    )
+
+    threat_analysis(
+        enemy_players,
+        hero_map
+    )
+
+    item_recommendation(
+        enemy_players,
+        hero_map
+    )
+
+## READ HISTORY
+
+def show_history():
+
+    print("\n====== ANALYSIS HISTORY ======")
+
+    history = view_history()
+
+
+    for record in history:
+
+        print(f"\nID: {record[0]}")
+        print(f"Match ID: {record[1]}")
+        print(f"Hero: {record[2].title()}")
+        print(f"Player: {record[3]}")
+        print(f"KDA: {record[4]}")
+        print(f"LH/min: {record[5]}")
+        print(f"GPM: {record[6]}")
+        print(f"Status: {record[7]}")
+
+        print("-" * 30)
+
+## PERSONAL NOTES USER
+
+def update_reflection():
+
+    print("\n====== UPDATE REFLECTION ======")
+
+
+    record_id = int(
+        input("Enter Analysis ID: ")
+    )
+
+
+    note = input(
+        "Enter your reflection: "
+    )
+
+
+    update_note(
+        record_id,
+        note
+    )
+
+
+    print("Note updated.")
+
+## DELETE RECORD ANALYSIS
+
+def delete_record():
+
+    print("\n====== DELETE ANALYSIS ======")
+
+
+    record_id = int(
+        input("Enter Analysis ID: ")
+    )
+
+
+    delete_analysis(
+        record_id
+    )
+
+
+    print("Record deleted.")
+
+### DISPLAY MENU BUTTON
+
+def show_menu():
+
+    print("""
+====== DOTA POS 1 COACH ======
+
+1. Analyse New Match
+2. View History
+3. Update Reflection
+4. Delete Analysis
+5. Exit
+
+""")
+
+## MENU CONTROLLER ##
+
+if __name__ == "__main__":
+
+    add_notes_column()
+
+    create_table()
+
+
+    while True:
+
+        show_menu()
+
+
+        choice = input(
+            "Choose option: "
+        )
+
+
+        if choice == "1":
+
+            analyse_match()
+
+
+        elif choice == "2":
+
+            show_history()
+
+
+        elif choice == "3":
+
+            update_reflection()
+
+
+        elif choice == "4":
+
+            delete_record()
+
+
+        elif choice == "5":
+
+            print("Exit...")
+            break
+
+
+        else:
+
+            print("Invalid choice")
